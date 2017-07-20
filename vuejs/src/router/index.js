ccc0e02f1d114c6706821b4b8947c08b16710e07
lmaduranga <lilan.maduranga@gmail.com>
2017-07-20 22:43:14 +0530

    Updated the vuejs route files

--- vuejs/src/router/index.js
+++ vuejs/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Posts from '@/components/Posts'
+import Hello from '@/components/Hello'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/post',
       name: 'Post',
       component: Posts
+    },
+    {
+        path: '/post',
+        name: 'Home',
+        component: Hello
     }
   ]
 })
