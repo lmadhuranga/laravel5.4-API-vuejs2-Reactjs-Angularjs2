21c1c01ffb1ca04a8f94c43dbb3c940eb7d330a6
lmadhuranga <lilan.maduranga@gmail.com>
2018-11-15 05:37:04 +0530

    *Configure wallaby for the editors help for js-unit

--- AngularJs06/src/app/post/post.component.spec.ts
+++ AngularJs06/src/app/post/post.component.spec.ts
@@ -1,25 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing'
+import { from } from 'rxjs';
 
 import { PostComponent } from './post.component';
+import { PostService } from '../services/post.service';
 
 describe('PostComponent', () => {
   let component: PostComponent;
-  let fixture: ComponentFixture<PostComponent>;
+  let fixture: ComponentFixture<PostComponent>; 
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostComponent ]
+      declarations: [ PostComponent ], 
+      imports: [FormsModule, RouterTestingModule ]
     })
     .compileComponents();
   }));
-
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
+    component.postsList =  [
+      {
+        "id": 1,
+        "title": "Post 1",
+        "author": "Auth1"
+      },
+      {
+        "id": 2,
+        "title": "Post 2",
+        "author": "Auth1"
+      }
+    ];
+    component.post = {
+      title:'title',
+      author:'tet',
+      id:1
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create', () => {
+    expect(1+1).toEqual(3);
+  });
+
 });
